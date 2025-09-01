519f18fce4bee7ec4f58a4a3ad6da13589d4ab41
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 15:28:26 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,27 @@
 gsap.registerPlugin(ScrollTrigger)
+
+
+const navToggle = document.querySelector(".menu")
+
+
+const navTl = gsap.timeline({paused:true})
+
+navTl.to(".navigation",{height:"100dvh",ease:"power2.inOut",
+                 duration:.85}).from(".dLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
+              ease:"power2.inOut"},0.2).from(".hLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
+            ease:"power2.inOut"},0.2)
+
+
+
+let state = false;
+
+function switchMenu() {
+  state = !state
+  if(state){
+    navTl.play()
+  } else{
+    navTl.reverse()
+  }
+}
+ 
+navToggle.addEventListener("click",switchMenu())
