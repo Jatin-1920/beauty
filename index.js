50adff9f93ca1b9d5e921a832dd4964c712695f8
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 15:27:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,30 +70,7 @@ transformOrigin:"bottom"
 
 )
 
-const navToggle = document.querySelector(".menu")
-gsap.set(".navigation",{
-height:"0"
-})
 
-const navTl = gsap.timeline({paused:true})
-
-navTl.to(".navigation",{height:"100dvh",ease:"power2.inOut",
-                 duration:.85}).from(".dLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
-              ease:"power2.inOut"},0.2).from(".hLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
-            ease:"power2.inOut"},0.2)
-
-
-
-let state = false
-
-function switchMenu() {
-  state = !state
-  if(state){
-    navTl.play()
-  } else{
-    navTl.reverse()
-  }
-}
 
 
 
