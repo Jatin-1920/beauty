b20cb310be2bc4f2606b8211fabfe2374c53ba19
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 14:58:04 +0530

    j

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const motiveScroll = document.querySelector('.smoothScroll')
+const locoScroll = new LocomotiveScroll({
+  el:motiveScroll ,
+  lerp: 1,
+  gestureDirection:"both",
+  smartphone: {
+    smooth: true
+  }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(motiveScroll, {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  }, 
+  
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
+,
+  
+  
+/* pinType: motiveScroll.style.transform ? "transform": "fixed" 
+*/
+})
+
+gsap.fromTo[
+    ".card-trans",{
+y:"0%",
+z:0,
+rotationX:0
+    },{
+        y:"-50%",
+        z:-250,
+        rotationX:45,
+        scrollTrigger:{
+            trigger:".cardStick2",
+            start:"top 85%",
+            end:"top -75%",
+            scrub:true,
+            pin:".cardStick2",
+            pinSpacing:false
+        }
+    }
+]
+
+ 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+gsap.to[".mask-img",{
+y:"100%",
+ease:"power3.inOut",
+duration:.7,
+}
+
+]
