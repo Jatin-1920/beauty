1508ec621000692106399799eebe14fd3b73499c
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 21:37:56 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -12,8 +12,8 @@ navTl.to(".navigation",{height:"100dvh",ease:"power1.inOut",
                  duration:.85}).from(".dLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
               ease:"power2.inOut"},0.2).from(".hLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
             ease:"power1.inOut"},0.2).to(".smoothScroll",{
-y:"10vh",
-duration:1,
+y:"20vh",
+duration:.7,
 ease:"power3.inOut"
 
 },0)
