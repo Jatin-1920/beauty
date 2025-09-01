47df24df5a2e4b2463cc5f1e8eef212564f022ae
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 15:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,6 +70,31 @@ transformOrigin:"bottom"
 
 )
 
+const navToggle = document.querySelector(".menu")
+
+
+const navTl = gsap.timeline({paused:true})
+
+navTl.to(".navigation",{height:"100dvh",ease:"power2.inOut",
+                 duration:.85}).from(".dLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
+              ease:"power2.inOut"},0.2).from(".hLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
+            ease:"power2.inOut"},0.2)
+
+
+
+let state = false
+
+function switchMenu() {
+  state = !state
+  if(state){
+    navTl.play()
+  } else{
+    navTl.reverse()
+  }
+}
+
+
+
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update()); 
 
 ScrollTrigger.refresh(); 
