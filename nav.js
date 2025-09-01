61803a278ce24e8c67c3fb82e23f9ebde7b5db8f
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-09-01 21:35:04 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -11,7 +11,12 @@ const navTl = gsap.timeline({paused:true})
 navTl.to(".navigation",{height:"100dvh",ease:"power1.inOut",
                  duration:.85}).from(".dLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
               ease:"power2.inOut"},0.2).from(".hLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
-            ease:"power1.inOut"},0.2)
+            ease:"power1.inOut"},0.2).to(".smoothScroll",{
+y:"10vh",
+duration:1,
+ease:"power3.inOut"
+
+},0)
 
 
 
